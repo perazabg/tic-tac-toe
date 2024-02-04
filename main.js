2d44f6fc45b06dd05ac0814d74e976ecea8e65a7
perazabg <96875802+perazabg@users.noreply.github.com>
2024-02-03 16:26:12 -0800

    added html skeleton and button event listeners

---
+++ main.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById("startBtn");
+const restartBtn = document.getElementById("restartBtn");
+
+startBtn.addEventListener("click", e => {
+    console.log("hello");
+})
