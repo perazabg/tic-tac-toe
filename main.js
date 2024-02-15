be5e9be42cdc453dd5aa73061ef86b4093dccd3a
perazabg <96875802+perazabg@users.noreply.github.com>
2024-02-15 12:11:01 -0800

    some logic done some css added

--- main.js
+++ main.js
@@ -1,6 +1,57 @@
 const startBtn = document.getElementById("startBtn");
 const restartBtn = document.getElementById("restartBtn");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+
+const Gameboard = (() => {
+    let gameboard = ["","","","","","","","",""]
+
+    const render = () =>{
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square${index}">${square}</div>`
+        })
+        document.getElementById("gameboard").innerHTML = boardHTML;
+    };
+
+    
+
+    return {
+        render,
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver = false;
+
+    const start = () => {
+        players = [
+            createPlayer(document.getElementById("player1").value, "X"),
+            createPlayer(document.getElementById("player2").value, "O")
+        ]
+        
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    };
+    return {
+        start,
+    }
+})();
 
 startBtn.addEventListener("click", e => {
-    console.log("hello");
-})
+    Game.start();
+});
+
+restartBtn.addEventListener("click", e =>{
+    Game.restart();
+});
