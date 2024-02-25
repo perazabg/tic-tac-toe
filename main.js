dc19704d97a9024ec629d797c28920091a022038
perazabg <96875802+perazabg@users.noreply.github.com>
2024-02-25 02:36:48 -0800

    added the update object function, restart function, and checkwin function

--- main.js
+++ main.js
@@ -18,10 +18,17 @@ const Gameboard = (() => {
         })
     };
 
-    
+    const update = (index, value) =>{
+        gameboard[index] = value;
+        render();
+    };
+
+    const getGameboard = () => gameboard;
 
     return {
         render,
+        update,
+        getGameboard
     }
 })();
 
@@ -46,16 +53,61 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+        })
     };
 
+    const restart = () =>{
+        for(let i = 0; i < 9; i++){
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
+    }
+
     const handleClick = (e) => {
-        console.log(e.target.id);
+        let index = parseInt(e.target.id.split("square")[1]);
+        
+        if(Gameboard.getGameboard()[index] !== ""){
+            return;
+        }
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+
+        if(checkWin(Gameboard.getGameboard())){
+            alert(`${players[currentPlayerIndex].name} wins!`);
+            gameOver = true;
+            Game.restart();
+        }
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
     return {
         start,
+        restart,
         handleClick
     }
 })();
+
+function checkWin(board){
+    let winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for(let i = 0; i < winCombos.length; i++){
+        let [a, b, c] = winCombos[i];
+        if(board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true;
+        }
+    }
+    return false;
+}
 
 startBtn.addEventListener("click", () => {
     Game.start();
