c87d86843343706637611f2c1970522cc18c2950
perazabg <96875802+perazabg@users.noreply.github.com>
2024-03-04 17:49:03 -0800

    converted objects to classes

--- main.js
+++ main.js
@@ -1,150 +1,139 @@
-const startBtn = document.getElementById("startBtn");
-const restartBtn = document.getElementById("restartBtn");
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
-const gameStatus = document.getElementById("gameStatus");
+class Gameboard {
+    constructor() {
+        this.gameboard = ["", "", "", "", "", "", "", "", ""];
+    }
 
-const Gameboard = (() => {
-    let gameboard = ["","","","","","","","",""];
-
-    const render = () =>{
+    render() {
         let boardHTML = "";
-        gameboard.forEach((square, index) => {
-            boardHTML += `<div class="square" id="square${index}">${square}</div>`
-        })
+        this.gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square${index}">${square}</div>`;
+        });
         document.getElementById("gameboard").innerHTML = boardHTML;
         const squares = document.querySelectorAll(".square");
         squares.forEach((square) => {
-            square.addEventListener("click", Game.handleClick);
-        })
-    };
-
-    const update = (index, value) =>{
-        gameboard[index] = value;
-        render();
-    };
-
-    const getGameboard = () => gameboard;
-
-    return {
-        render,
-        update,
-        getGameboard
+            square.addEventListener("click", this.handleSquareClick.bind(this));
+        });
     }
-})();
 
-const createPlayer = (name, mark, score) => {
-    return {
-        name,
-        mark,
-        score
+    update(index, value) {
+        this.gameboard[index] = value;
+        this.render();
     }
-}
 
-const Game = (() => {
-    let players = [];
-    let currentPlayerIndex;
-    let gameOver = false;
+    getGameboard() {
+        return this.gameboard;
+    }
 
-    const start = (e) => {
-        players = [
-            createPlayer(document.getElementById("player1").value, "X", 0),
-            createPlayer(document.getElementById("player2").value, "O", 0)
-        ]
-        renderResults();
-        currentPlayerIndex = 0;
-        gameOver = false;
-        Gameboard.render();
-        const squares = document.querySelectorAll(".square");
-        squares.forEach((square) => {
-            square.addEventListener("click", handleClick);
-        })
-        document.getElementById("playerTurn").innerText = players[currentPlayerIndex].name + "'s Turn";
-        document.getElementById("gameStatus").innerText = "Let's Play!";
-        
-    };
-
-    const restart = () =>{
-        currentPlayerIndex = 0;
-        for(let i = 0; i < 9; i++){
-            Gameboard.update(i, "");
-        }
-        Gameboard.render();
-        document.getElementById("player1Score").innerText = 0;
-        document.getElementById("player2Score").innerText = 0;
-        gameOver = false;
-    };
-
-    const handleClick = (e) => {
+    handleSquareClick(e) {
         let index = parseInt(e.target.id.split("square")[1]);
-        
-        if(Gameboard.getGameboard()[index] !== ""){
+
+        if (this.gameboard[index] !== "") {
             return;
         }
 
-        Gameboard.update(index, players[currentPlayerIndex].mark);
-
-        if(checkWin(Gameboard.getGameboard())){
-            gameStatus.innerText = `${players[currentPlayerIndex].name} wins the round!`;
-            gameOver = true;
-            restart();
-            if(currentPlayerIndex === 0){
-                players[currentPlayerIndex].score++;
-                renderResults();
-            }else{
-                players[currentPlayerIndex].score++;
-                renderResults();
-            }
-            
-        
-            return;
-        }
-        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
-        document.getElementById("playerTurn").innerText = players[currentPlayerIndex].name + "'s Turn";
-        renderResults();
-    };
-
-    const renderResults = () => {
-        document.getElementById("player1Score").innerText = players[0].name + "'s Score: " + players[0].score;
-        document.getElementById("player2Score").innerText = players[1].name + "'s Score: " + players[1].score;
-    };
-
-    return {
-        start,
-        restart,
-        handleClick
+        game.handleClick(index);
     }
-})();
-
-function checkWin(board){
-    let winCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    if(board.every((square) => square !== "")){
-        return gameStatus.innerText = "DRAW", Game.restart();
-    }
-
-    for(let i = 0; i < winCombos.length; i++){
-        let [a, b, c] = winCombos[i];
-        if(board[a] && board[a] === board[b] && board[a] === board[c]){
-            return true;
-        }
-    }
-    return false;
 }
 
-startBtn.addEventListener("click", () => {
-    Game.start();
+class Player {
+    constructor(name, mark) {
+        this.name = name;
+        this.mark = mark;
+        this.score = 0;
+    }
+}
+
+class Game {
+    constructor() {
+        this.players = [];
+        this.currentPlayerIndex = 0;
+        this.gameOver = false;
+        this.gameboard = new Gameboard();
+    }
+
+    start() {
+        this.players = [
+            new Player(document.getElementById("player1").value, "X"),
+            new Player(document.getElementById("player2").value, "O")
+        ];
+        this.renderResults();
+        this.currentPlayerIndex = 0;
+        this.gameOver = false;
+        this.gameboard.render();
+        document.getElementById("gameStatus").innerText = "Let's Play!";
+    }
+
+    restart() {
+        this.currentPlayerIndex = 0;
+        for (let i = 0; i < 9; i++) {
+            this.gameboard.update(i, "");
+        }
+        document.getElementById("gameStatus").innerText = "Let's Play!";
+        this.renderResults();
+        this.gameOver = false;
+    }
+
+    handleClick(index) {
+        this.gameboard.update(index, this.players[this.currentPlayerIndex].mark);
+
+        if (this.checkWin(this.gameboard.getGameboard())) {
+            document.getElementById("gameStatus").innerText = `${this.players[this.currentPlayerIndex].name} wins the round!`;
+            this.gameOver = true;
+            this.restart();
+            if (this.currentPlayerIndex === 0) {
+                this.players[this.currentPlayerIndex].score++;
+                this.renderResults();
+            } else {
+                this.players[this.currentPlayerIndex].score++;
+                this.renderResults();
+            }
+            return;
+        }
+
+        if (this.gameboard.getGameboard().every((square) => square !== "")) {
+            document.getElementById("gameStatus").innerText = "DRAW";
+            this.restart();
+            return;
+        }
+
+        this.currentPlayerIndex = this.currentPlayerIndex === 0 ? 1 : 0;
+        document.getElementById("gameStatus").innerText = `${this.players[this.currentPlayerIndex].name}'s Turn`;
+    }
+
+    checkWin(board) {
+        let winCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winCombos.length; i++) {
+            let [a, b, c] = winCombos[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    renderResults() {
+        document.getElementById("player1Score").innerText = `${this.players[0].name}'s Score: ${this.players[0].score}`;
+        document.getElementById("player2Score").innerText = `${this.players[1].name}'s Score: ${this.players[1].score}`;
+    }
+}
+
+const game = new Game();
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    game.start();
 });
 
-restartBtn.addEventListener("click", () =>{
-    Game.restart();
+document.getElementById("restartBtn").addEventListener("click", () => {
+    game.restart();
 });
